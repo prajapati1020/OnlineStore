c9ac185f553f7c0af854d4f9f8826fd692eb0352
arprajap <arjitprajapati@gmail.com>
2026-06-10 17:37:58 +0530

    preventing large purchase,setting limit.

--- src/main/java/online/store/globalException/GlobalExceptionHandling.java
+++ src/main/java/online/store/globalException/GlobalExceptionHandling.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
 public class GlobalExceptionHandling {
 
@@ -13,5 +15,18 @@ public class GlobalExceptionHandling {
     public ResponseEntity<String> creditCardValidationFailed(CreditCardValidationException e){
         System.out.println(String.format("Request to /checkout path threw an exception %s",e.getMessage()));
         return new ResponseEntity<String>("Credit card is invalid ,please use another form of payment", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> defaultErrorHandler(Exception e , HttpServletRequest request) throws Exception{
+
+        System.out.println(String.format("Exception in handling request to %s:%s",
+                request.getRequestURI(),e.getMessage()));
+
+        e.printStackTrace();
+
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+
+
     }
 }
--- src/main/java/online/store/services/OrderService.java
+++ src/main/java/online/store/services/OrderService.java
@@ -2,18 +2,38 @@ package online.store.services;
 
 import online.store.model.Order;
 import online.store.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final long maxNumberOfItems;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository,
+                        @Value("${product.service.max-number-of-items:25}") long maxNumberOfItems) {
+
         this.orderRepository = orderRepository;
+        this.maxNumberOfItems = maxNumberOfItems;
     }
 
     public void placeOrders(Iterable<Order> orders){
+        validateNumberOfItemsOrdered(orders);
         orderRepository.saveAll(orders);
+    }
+
+    private void validateNumberOfItemsOrdered(Iterable<Order> order){
+        long totalNumbersOfItems=0;
+
+        for(Order orders : order){
+            totalNumbersOfItems +=orders.getQuantity();
+
+            if(totalNumbersOfItems>maxNumberOfItems){
+                throw new IllegalStateException(String.format
+                        ("Number of Products %d exceeded the limit of %d",
+                                totalNumbersOfItems, maxNumberOfItems));
+            }
+        }
     }
 }
